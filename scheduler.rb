29125cfc007f386b3edf36d55ee2941c9771e6ca
Gaston Ramos <ramos.gaston@gmail.com>
2024-07-15 11:46:32 -0300

    Frist full version working ok

--- scheduler.rb
+++ scheduler.rb
@@ -1,36 +1,73 @@
 class Scheduler
-  def self.q_changed(shows_api, shows_db)
-    shows_db.map do |s|
-      s_api = shows_api.select{|sa|
-        sa[:id] == s[:id]
-      }.first
 
-      s if s[:quantity] != s_api[:quantity]
-    end.compact
-  end
+  class << self
+    @@update_interval = 15
+    @@timeout         = 3600
 
-  def self.to_hour(time)
-    ( (time / 60.0) / 60.0 ).round(2)
-  end 
-
-  def self.one_h_outdated(show_list)
-    show_list.map do |s|
-      s if to_hour(s[:last_update]) >= 1
-    end.compact
-  end
-
-  def self.run(shows_api, shows_db)
-    shows = q_changed(shows_api, shows_db) && one_h_outdated(shows_db)
-
-    l = {}
-    update_time = 0
-
-    shows.each do |s|
-      l[ s[:id] ] = update_time
-      update_time += 15
+    def timeout
+      @@timeout
     end
 
-    l
+    def update_interval
+      @@update_interval
+    end
+
+    def update_interval=(time)
+      @@update_interval = time
+    end
+    
+    def timeout=(time)
+      @@timeout = time
+    end
+
+    def total_time(shows)
+      shows.values.reduce{ |sum, t| sum + t }
+    end
+    
+    def q_changed(shows_api, shows_db)
+      shows_db.map do |s|
+        s_api = shows_api.select{|sa|
+          sa[:id] == s[:id]
+        }.first
+
+        s if s[:quantity] != s_api[:quantity]
+      end.compact
+    end
+
+    def to_hour(time)
+      ( (time / 60.0) / 60.0 ).round(2)
+    end 
+
+    def one_h_outdated(show_list)
+      show_list.map do |s|
+        s if to_hour(s[:last_update]) >= 1
+      end.compact
+    end
+
+    def calculate_jobs(shows_api, shows_db)
+      shows = q_changed(shows_api, shows_db) && one_h_outdated(shows_db)
+
+      l = {}
+      update_time = 0
+
+      shows.each do |s|
+        l[ s[:id] ] = update_time
+        update_time += update_interval
+      end
+
+      l
+    end
+    
+    def run(shows_api, shows_db)
+      jobs = calculate_jobs(shows_api, shows_db)
+
+      while(total_time(jobs) > timeout)
+        Scheduler.update_interval -= 1
+        jobs = calculate_jobs(shows_api, shows_db)
+      end
+
+      jobs
+    end
   end
 end
 
@@ -91,6 +128,21 @@ describe Scheduler do
       shows_needs_update = {2=>0, 3=>15, 4=>30, 5=>45}
       _(Scheduler.run(RemoteApi.get, Show.all) ).must_equal shows_needs_update
     end
+
+    it "should reduce the update time interval when the sum exceeds 1 hour,
+       that means, 240 shows of 15 segs" do
+      shows_needs_update = {2=>0, 3=>15, 4=>30, 5=>45}
+      
+      Scheduler.update_interval = 15
+      Scheduler.timeout         = 30
+      
+      _(Scheduler.total_time(shows_needs_update)).must_be :>=, Scheduler.timeout
+      shows_with_less_interval = {2=>0, 3=>5, 4=>10, 5=>15}
+
+      _(Scheduler.run(RemoteApi.get, Show.all)).must_equal shows_with_less_interval
+
+      Scheduler.update_interval = 15
+      Scheduler.timeout         = 3600
+    end
   end
 end
-
